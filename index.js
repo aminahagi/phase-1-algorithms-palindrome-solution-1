e662f57074b948503cac3a08cd1ec1313ea60a36
aminahagi <amina.hagi@student.moringaschool>
2022-12-13 14:31:30 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+ 
+/* 
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
+/*
+  Add written explanation of your solution here
+*/
 
+// You can run `node index.js` to view these console logs
+
+function reverse(word) {
+  // Write your algorithm here
+  return word.split("").reverse().join("");
+}
+
+
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+  return word === reversedWord; 
+}
 /* 
   Add your pseudocode here
 */
-
+//we need to split the words given
+//we need reverse
+//we need to join
+//return the code and find out whether they are palindrome or not a palindrome 
 /*
+
   Add written explanation of your solution here
 */
 
@@ -20,6 +48,7 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
+  }
 
-module.exports = isPalindrome;
+  module.exports = isPalindrome;
+
